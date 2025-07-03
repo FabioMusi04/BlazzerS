fdda4eaf9f1e7486cc4ac1f1d79abc3e2f0e15c8
FabioMusi04 <76118020+FabioMusi04@users.noreply.github.com>
2025-07-03 22:06:59 +0200

    feat(home page) and refactor layout

---
+++ Models/Comment.cs
@@ -0,0 +1,10 @@
+﻿namespace Models
+{
+    public class Comment
+    {
+        public string Author { get; set; } = "";
+        public string AvatarUrl { get; set; } = "";
+        public string Content { get; set; } = "";
+        public DateTime Timestamp { get; set; }
+    }
+}
---
+++ Models/Post.cs
@@ -0,0 +1,13 @@
+﻿namespace Models
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public string Author { get; set; } = "";
+        public string AvatarUrl { get; set; } = "";
+        public DateTime Timestamp { get; set; }
+        public string Content { get; set; } = "";
+        public int Replies { get; set; }
+        public int Likes { get; set; }
+    }
+}
